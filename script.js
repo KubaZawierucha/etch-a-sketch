6af54a114fa21f869a956622842b0402594c94b3
Kuba Zawierucha <j.zawierucha97@gmail.com>
2020-01-31 09:35:16 +0100

    Color picker added. Project finished.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const screenWeight = 1600;
 let isDown = false;
 
 const container = document.querySelector(".container");
+const colorPicker = document.querySelector("#colorPicker");
+colorPicker.addEventListener("change", watchColor, false);
 
 generateGrid();
 
@@ -37,7 +39,7 @@ function generateGrid() {
     
         innerDiv.style.width = screenHeight / numOfDivsInRow + "px";
         innerDiv.style.height = screenHeight / numOfDivsInRow + "px";
-    
+
         innerDiv.addEventListener('mouseover', changeColor);
         innerDiv.addEventListener('click', changeColorWithClick);
 
@@ -53,23 +55,40 @@ window.addEventListener("mouseup", () => {
     isDown = false;
 })
 
+window.addEventListener('load', (e) => {
+    color = colorPicker.value;
+})
+
 function changeColor(e) {
     if (isDown) {
-        if (e.target.style.opacity !== "") {
-            e.target.style.opacity = +(e.target.style.opacity) + 0.5;
-        } else {
-            e.target.style.backgroundColor = "green";
-            e.target.style.opacity = 0.2;
-        }
+        e.target.style.backgroundColor = color;
+
+        // Mechasnim of changing the opacity of the single square (i.e. you have to enter one square a few times to get it colored).
+        // However, it is pointless to use it, when we can select any color with the color picker.
+
+        // if (e.target.style.opacity !== "") {
+        //     e.target.style.opacity = +(e.target.style.opacity) + 0.5;
+        // } else {
+        //     e.target.style.backgroundColor = "green";
+        //     e.target.style.opacity = 0.2;
+        // }
     }
 }
 
 function changeColorWithClick(e) {
-    if (e.target.style.opacity !== "") {
-        e.target.style.opacity = +(e.target.style.opacity) + 0.5;
-    } else {
-        e.target.style.backgroundColor = "green";
-        e.target.style.opacity = 0.2;
-    }
+    e.target.style.backgroundColor = color;
+
+    // Mechasnim of changing the opacity of the single square (i.e. you have to enter one square a few times to get it colored).
+    // However, it is pointless to use it, when we can select any color with the color picker.
+
+    // if (e.target.style.opacity !== "") {
+    //     e.target.style.opacity = +(e.target.style.opacity) + 0.5;
+    // } else {
+    //     e.target.style.backgroundColor = "green";
+    //     e.target.style.opacity = 0.2;
+    // }
 }
 
+function watchColor(e) {
+    color = e.target.value;
+}
